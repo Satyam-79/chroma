6d06366b69640b6f3f56c7c567c3dd569a5c1523
Satyam-79 <satyamverma79@gmail.com>
2023-05-20 12:15:56 +0530

    added google_vertex in ef

---
+++ chromadb/ef/google_vertex.py
@@ -0,0 +1,28 @@
+from chromadb.api.types import Documents, EmbeddingFunction, Embeddings
+import requests
+
+
+class GoogleVertexEmbeddingFunction(EmbeddingFunction):
+    # Follow API Quickstart for Google Vertex AI
+    # https://cloud.google.com/vertex-ai/docs/generative-ai/start/quickstarts/api-quickstart
+    # Information about the text embedding modules in Google Vertex AI
+    # https://cloud.google.com/vertex-ai/docs/generative-ai/embeddings/get-text-embeddings
+    def __init__(
+        self,
+        api_key: str,
+        model_name: str = "textembedding-gecko-001",
+        project_id: str = "cloud-large-language-models",
+        region: str = "us-central1",
+    ):
+        self._api_url = f"https://{region}-aiplatform.googleapis.com/v1/projects/{project_id}/locations/{region}/endpoints/{model_name}:predict"
+        self._session = requests.Session()
+        self._session.headers.update({"Authorization": f"Bearer {api_key}"})
+
+    def __call__(self, texts: Documents) -> Embeddings:
+        response = self._session.post(
+            self._api_url, json={"instances": [{"content": texts}]}
+        ).json()
+
+        if "predictions" in response:
+            return response["predictions"]  # type: ignore
+        return []
